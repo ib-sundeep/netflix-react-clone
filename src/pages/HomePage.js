49df04d937525fbe7dcbb81802fdbc2ed34a68ed
Sundeep Babbur <sundeep@interviewbit.com>
2021-10-10 20:19:11 +0530

    Adds banner component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Banner from 'ui/Banner';
 import Header from 'ui/Header';
 
 function HomeScreen() {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   );
 }
--- src/ui/Banner.js
+++ src/ui/Banner.js
@@ -4,7 +4,39 @@ import { generateImageUrl, ImageSizes } from "utils/tmdb";
 import tmdbApi from 'api/tmdb';
 
 function Banner() {
-  return null;
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchMovie() {
+      const json = await tmdbApi.get('/discover/tv', { with_networks: 213 });
+      setMovie(json.results[0]);
+    }
+
+    fetchMovie();
+  }, []);
+
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundImage: `url(${generateImageUrl(movie.backdrop_path || '', ImageSizes.backdrop)})`
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <div className="banner__description">
+          {movie.overview}
+        </div>
+      </div>
+      <div className="banner__cover" />
+    </div>
+  );
 }
 
 export default Banner;
